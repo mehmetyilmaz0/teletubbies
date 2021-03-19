bcab9810cb4474bb23771e445c2e4add4c5734e3
mehmet.yilmaz <sam@google.com>
2021-03-19 19:20:56 +0300

    add methods in some repositories

--- src/main/java/com/mehmetyilmaz/teletubbies/repository/ProjectRepository.java
+++ src/main/java/com/mehmetyilmaz/teletubbies/repository/ProjectRepository.java
@@ -1,14 +1,21 @@
 package com.mehmetyilmaz.teletubbies.repository;
 
 import com.mehmetyilmaz.teletubbies.entity.Project;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     List<Project> getAllByProjectNameNotNull ();
+
+    Page<Project> findAll (Pageable pageable);
+
+    List<Project> findAll (Sort sort);
 
 }
